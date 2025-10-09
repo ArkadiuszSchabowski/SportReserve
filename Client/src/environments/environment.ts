e077dfdaa713cebed25ad667296a38de3865da35
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-10-09 18:12:35 +0200

    feat: add environment files

---
+++ Client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    docker: false,
+    apiUrl: 'http://localhost:5000/',
+    raceUrl: 'http://localhost:5002/',
+    reservationUrl: 'http://localhost:5004/'
+  };
