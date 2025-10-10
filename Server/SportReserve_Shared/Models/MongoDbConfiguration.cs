743027ba84392c64876b3fad0cae2a5381cbeb6a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-10-10 23:07:09 +0200

    feat: add MongoDbConfiguration model

---
+++ Server/SportReserve_Shared/Models/MongoDbConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SportReserve_Shared.Models
+{
+    public class MongoDbConfiguration
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
