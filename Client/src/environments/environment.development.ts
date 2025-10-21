534b5e1d6457f74c09f701bcf874d71c11b52a4f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-10-21 18:15:11 +0200

    refactor: update environments

---
+++ Client/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    docker: false,
+    apiUrl: 'http://localhost:5000/api/',
+    raceUrl: 'http://localhost:5002/api/',
+    reservationUrl: 'http://localhost:5004/api/'
+  };
--- Client/src/environments/environment.docker.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  docker: true,
-  apiUrl: '/api/',
-  raceUrl: '/race/',
-  reservationUrl: '/reservation/'
-};
